fb659610a7f4f6b2aa43edda2b1ad601e09fa086
Nhật Vũ <nhatvu148@gmail.com>
2022-10-03 23:03:09 +0900

    add web-app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Browse the docs locall and offline
+
+- rustup component add rust-docs
+- rustup doc --std
+- cargo doc --open
---
+++ web-app/src/main.rs
@@ -0,0 +1,29 @@
+// ch02/src/main.rs
+#[warn(dead_code)]
+ 
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+ 
+#[derive(Debug)]
+struct QuestionId(String);
+ 
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+ 
+fn main() {
+    let question = Question::new(QuestionId("1".to_string()), "First Question".to_string(), "Content of question".to_string(), Some(vec!("faq".to_string())));
+    println!("{:?}", question);
+}
